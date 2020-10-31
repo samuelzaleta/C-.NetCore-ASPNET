d6e4fa79bdfad6000f3298b7370243271779fadb
Samuel Zaleta <samuel.zaleta.cbtis103@gmail.com>
2020-10-30 21:24:31 -0600

    EscuelaCore

--- CoreEscuela/Program.cs
+++ CoreEscuela/Program.cs
@@ -2,11 +2,29 @@
 
 namespace CoreEscuela
 {
+
+    class CoreEscuela
+    {
+        public String nombreEscuela;
+        public String direccion;
+        public int añoFundacion;
+        public void Timbrar()
+        {
+            //do
+            Console.Beep(20000,3000);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var miEscuela2 = new CoreEscuela();
+            miEscuela2.nombreEscuela = "Platzi";
+            miEscuela2.direccion= "El rio";
+            miEscuela2.añoFundacion= 1999;
+            miEscuela2.Timbrar();
+            Console.WriteLine("Timbrando");
+            //Console.WriteLine("Hello World!");
         }
     }
 }
