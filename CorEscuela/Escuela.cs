50103f3855eb1421689868079f578ac6a1ed7cf3
Samuel Zaleta <samuel.zaleta.cbtis103@gmail.com>
2020-11-01 18:08:54 -0600

    ggg

---
+++ CorEscuela/Escuela.cs
@@ -0,0 +1,26 @@
+﻿namespace CoreEscuela.Entidades
+{
+    class CoreEscuela
+    {
+        string nombre;
+        public string Nombre{
+            get{return "Copia" +=nombre;}
+            set{nombre = value.ToUpper;}
+        }
+
+        public int AñoCreacion {get; set;}
+
+        public string Pais {get; set;}
+
+        public string Ciuda {get; set;}
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get {return myVar}
+            set {myVar = value;}
+        }
+
+    }
+}
